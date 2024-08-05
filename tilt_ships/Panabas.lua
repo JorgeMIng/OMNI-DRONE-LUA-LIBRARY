8b25e827477d04eddb451e136a12d4773c5ce179
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-08-05 19:53:31 +0800

    house-keeping

--- tilt_ships/Panabas.lua
+++ tilt_ships/Panabas.lua
@@ -1,40 +1,11 @@
 local quaternion = require "lib.quaternions"
-local utilities = require "lib.utilities"
-local targeting_utilities = require "lib.targeting_utilities"
-local player_spatial_utilities = require "lib.player_spatial_utilities"
 local flight_utilities = require "lib.flight_utilities"
-local list_manager = require "lib.list_manager"
+
 
 local DroneBaseClassKontraption = require "lib.tilt_ships.DroneBaseClassKontraption"
 local Object = require "lib.object.Object"
 
-local sqrt = math.sqrt
-local abs = math.abs
-local max = math.max
-local min = math.min
-local mod = math.fmod
-local cos = math.cos
-local sin = math.sin
-local acos = math.acos
-local pi = math.pi
-local clamp = utilities.clamp
-local sign = utilities.sign
-
-local quadraticSolver = utilities.quadraticSolver
-local getTargetAimPos = targeting_utilities.getTargetAimPos
-local getQuaternionRotationError = flight_utilities.getQuaternionRotationError
 local getLocalPositionError = flight_utilities.getLocalPositionError
-local adjustOrbitRadiusPosition = flight_utilities.adjustOrbitRadiusPosition
-local getPlayerLookVector = player_spatial_utilities.getPlayerLookVector
-local getPlayerHeadOrientation = player_spatial_utilities.getPlayerHeadOrientation
-local rotateVectorWithPlayerHead = player_spatial_utilities.rotateVectorWithPlayerHead
-local PlayerVelocityCalculator = player_spatial_utilities.PlayerVelocityCalculator
-local RadarSystems = targeting_utilities.RadarSystems
-local TargetingSystem = targeting_utilities.TargetingSystem
-local IntegerScroller = utilities.IntegerScroller
-local NonBlockingCooldownTimer = utilities.NonBlockingCooldownTimer
-local IndexedListScroller = list_manager.IndexedListScroller
-
 
 local Panabas = Object:subclass()
 
@@ -194,7 +165,7 @@ function Panabas:overrideShipFrameCustomFlightLoopBehavior()
 			self.error
 		]]--
 		self.target_global_velocity = vector.new(0,0,0)
-		
+
 		if (self.remoteControlManager.rc_variables.run_mode) then
 			return
 		end
